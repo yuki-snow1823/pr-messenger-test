0dec3d53c79147d67413268e906bd04c88f16461
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2025-06-11 23:32:56 +0900

    message

--- .github/scripts/post_to_x.ts
+++ .github/scripts/post_to_x.ts
@@ -13,8 +13,8 @@ const prTitle = pr.title || 'PRタイトル';
 const prUrl = pr.html_url || 'PRリンク';
 const prUser = (pr.user && pr.user.login) || 'ユーザー名';
 
-// const text = `PRがマージされました！\nタイトル: ${prTitle}\nリンク: ${prUrl}\n提案者: ${prUser}`;
-const text = `test`;
+const text = `提案したPRがマージされました！\nタイトル: ${prTitle}\nリンク: ${prUrl}\n提案者: ${prUser}さん`;
+// const text = `test`;
 
 const client = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY!,
